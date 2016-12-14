b92fb7ca6e26efc61699de7af1933cfbf89b09bd
wnhh <wnax1314@163.com>
2016-12-14 18:30:26 +0800

    chongzhi  tiaozhuan

--- app/src/main/java/com/yztc/lovetv/activity/mine/ChongzhiActivity.java
+++ app/src/main/java/com/yztc/lovetv/activity/mine/ChongzhiActivity.java
@@ -1,15 +1,41 @@
 package com.yztc.lovetv.activity.mine;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.yztc.lovetv.R;
 
 public class ChongzhiActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chongzhi);
+        initToolbar();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                  finish();
+                break;
+        }
+
+        return true;
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar_chongzhi);
+        //使用ActionBar的导航按钮
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        //添加返回按钮
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/yztc/lovetv/fragment/tabhost/MineFragment.java
+++ app/src/main/java/com/yztc/lovetv/fragment/tabhost/MineFragment.java
@@ -1,31 +1,45 @@
 package com.yztc.lovetv.fragment.tabhost;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.yztc.lovetv.R;
+import com.yztc.lovetv.activity.mine.ChongzhiActivity;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MineFragment extends Fragment {
+public class MineFragment extends Fragment implements View.OnClickListener{
 
-
-    public MineFragment() {
-        // Required empty public constructor
-    }
-
+    Button chongzhiBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        View v = inflater.inflate(R.layout.fragment_mine, container, false);
+        initView(v);
+        return v;
     }
 
+    private void initView(View v) {
+        chongzhiBtn = (Button) v.findViewById(R.id.chongzhi_btn);
+        chongzhiBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.chongzhi_btn:
+                Intent intent = new Intent(getActivity(), ChongzhiActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
