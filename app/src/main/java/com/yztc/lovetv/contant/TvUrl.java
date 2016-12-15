4644a0b18965bdddfcaa7b8c2f9db81d00303b5b
wnhh <wnax1314@163.com>
2016-12-15 20:14:32 +0800

    Url Contants

--- app/src/main/java/com/yztc/lovetv/contant/TvUrl.java
+++ app/src/main/java/com/yztc/lovetv/contant/TvUrl.java
@@ -5,5 +5,58 @@ package com.yztc.lovetv.contant;
  */
 
 public class TvUrl {
-	public static final String TUIAJIN="http://www.quanmin.tv/json/app/index/recommend/";
+	//推荐
+	public static final String TUIAJIN = "http://www.quanmin.tv/json/app/index/recommend/";
+	//英雄联盟
+	public static final String LOL ="http://www.quanmin.tv/json/categories/lol/list.json?11212123&v=2.2.4&os=1&ver=4";
+	//全民星秀
+	public static final String SHOW = "http://www.quanmin.tv/json/categories/beauty/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//炉石传说
+	public static final String LUSHI = "http://www.quanmin.tv/json/categories/heartstone/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//守望先锋
+	public static final String SHOUWANG = "http://www.quanmin.tv/json/categories/overwatch/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//球球大作战
+	public static final String QIUQIU = "http://www.quanmin.tv/json/categories/qiuqiu/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//穿越火线
+	public static final String CF = "http://www.quanmin.tv/json/categories/cfpc/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//QQ飞车
+	public static final String FEICHE = "http://www.quanmin.tv/json/categories/qqfeiche/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//NBA2K
+	public static final String KONLINE = "http://www.quanmin.tv/json/categories/nba2k/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//FIFA3
+	public static final String FIFA = "http://www.quanmin.tv/json/categories/fifa/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//魔兽争霸3
+	public static final String WOW = "http://www.quanmin.tv/json/categories/war3/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//王者荣耀
+	public static final String WANGZHE = "http://www.quanmin.tv/json/categories/wangzhe/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//网络游戏
+	public static final String WANGYOU = "http://www.quanmin.tv/json/categories/webgame/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//DNF
+	public static final String DNF = "http://www.quanmin.tv/json/categories/dnf/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//暴雪经典
+	public static final String BAOFENG = "http://www.quanmin.tv/json/categories/blizzard/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//DOTA2
+	public static final String DOTA = "http://www.quanmin.tv/json/categories/dota2/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//单机游戏
+	public static final String DANJI = "http://www.quanmin.tv/json/categories/tvgame/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//二次元
+	public static final String ERCIYUAN = "http://www.quanmin.tv/json/categories/erciyuan/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//手游专区
+	public static final String SHOUYOU = "http://www.quanmin.tv/json/categories/mobilegame/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//全民户外
+	public static final String HUWAI = "http://www.quanmin.tv/json/categories/huwai/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//颜值控
+	public static final String YANZHI = "http://www.quanmin.tv/json/categories/love/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//阴阳师
+	public static final String YINYANG = "http://www.quanmin.tv/json/categories/yys/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//街蓝专区
+	public static final String JIELAN = "http://www.quanmin.tv/json/categories/fs/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//劲舞团
+	public static final String JINWU = "http://www.quanmin.tv/json/categories/au/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//装甲风暴
+	public static final String ZHUANGJIA = "http://www.quanmin.tv/json/categories/zhuangjiafengbao/list.json?11212128&v=2.2.4&os=1&ver=4";
+	//传奇专区
+	public static final String CHUANGQI = "http://www.quanmin.tv/json/categories/chuanqi/list.json?11212128&v=2.2.4&os=1&ver=4";
+
+
 }
