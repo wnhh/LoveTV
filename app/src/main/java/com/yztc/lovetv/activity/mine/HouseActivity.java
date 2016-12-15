08b98f5239dbe45dcd68568910757538cb112cc4
wnhh <wnax1314@163.com>
2016-12-15 19:57:05 +0800

    wnupdate 主页面fragment

--- app/src/main/java/com/yztc/lovetv/activity/mine/HouseActivity.java
+++ app/src/main/java/com/yztc/lovetv/activity/mine/HouseActivity.java
@@ -1,11 +1,7 @@
 package com.yztc.lovetv.activity.mine;
 
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,27 +9,47 @@ import android.widget.TextView;
 
 import com.yztc.lovetv.R;
 
-public class HouseActivity extends AppCompatActivity {
-    ImageView ivBack;
-    Button tvLive;
+public class HouseActivity extends AppCompatActivity implements View.OnClickListener {
+	private ImageView ivBack;
+	private TextView tvLive;
+	private TextView tv_busee;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_house);
-        initView();
-        ivBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_house);
+		initView();
+		ivBack.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				finish();
+			}
+		});
+	}
 
-    private void initView() {
-        ivBack = (ImageView) findViewById(R.id.iv_house_back);
-        tvLive = (Button) findViewById(R.id.tv_see);
-    }
+	private void initView() {
+		ivBack = (ImageView) findViewById(R.id.iv_house_back);
+		tvLive = (TextView) findViewById(R.id.tv_see);
+		tv_busee = (TextView) findViewById(R.id.tv_busee);
+		tvLive.setOnClickListener(this);
+		tv_busee.setOnClickListener(this);
+		tvLive.setSelected(true);
+		tv_busee.setSelected(false);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.tv_busee:
+				tv_busee.setSelected(true);
+				tvLive.setSelected(false);
+				break;
+			case R.id.tv_see:
+				tvLive.setSelected(true);
+				tv_busee.setSelected(false);
+				break;
+		}
+	}
 }
 
